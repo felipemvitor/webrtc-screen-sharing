94be0018c36f895f4b63e7415eacf0e47949eeec
felipemvitor <felipe.martinsvitor@gmail.com>
2019-08-28 10:36:18 -0300

    Socket-io replaced by ws.

--- signaling-server/index.js
+++ signaling-server/index.js
@@ -1,22 +1,15 @@
-const http = require('http')
-const socketIo = require('socket.io')
+const WebSocket = require('ws')
 
-const PORT = 3000
+const wss = new WebSocket.Server({ port: 3000 })
 
-var server = http.createServer((req, res) => {
-    console.log("Server is running on port " + PORT)
-}).listen(3000)
-
-var webSocket = socketIo.listen(server)
-
-webSocket.sockets.on('connection', (socket) => {
+wss.on('connection', ws => {
     console.log(' User connected')
 
-    socket.on('message', message => {
+    ws.on('message', message => {
         console.log('Received message: ' + message)
     })
 
-    socket.on('close', () => {
-
+    ws.on('close', () => {
+        console.log('Close connection')
     })
 })
